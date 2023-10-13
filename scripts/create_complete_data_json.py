7ab93985e0fcdfe64442b59ecbd891a045cbc125
Niema Moshiri <niemamoshiri@gmail.com>
2023-10-13 15:25:34 -0700

    Update create_complete_data_json.py

--- scripts/create_complete_data_json.py
+++ scripts/create_complete_data_json.py
@@ -5,10 +5,10 @@ Build a complete data.json file from game folders
 from glob import glob
 from json import dump as jdump
 data = dict()
-for game_folder in glob('games/*'):
+for game_folder in sorted(glob('games/*')):
     serial = game_folder.rstrip('/').split('/')[-1].strip()
     curr = dict()
-    for txt in glob('%s/*.txt' % game_folder):
+    for txt in sorted(glob('%s/*.txt' % game_folder)):
         curr[txt.split('/')[-1].split('.txt')[0].strip()] = open(txt).read().strip()
     data[serial] = curr
 f = open('data.json', 'w')
