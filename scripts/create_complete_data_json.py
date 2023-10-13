a6aee4344dc47da298084266e211eeeb97cf5659
Niema Moshiri <niemamoshiri@gmail.com>
2023-10-13 15:28:48 -0700

    Update create_complete_data_json.py

--- scripts/create_complete_data_json.py
+++ scripts/create_complete_data_json.py
@@ -9,7 +9,12 @@ for game_folder in sorted(glob('games/*')):
     serial = game_folder.rstrip('/').split('/')[-1].strip()
     curr = dict()
     for txt in sorted(glob('%s/*.txt' % game_folder)):
-        curr[txt.split('/')[-1].split('.txt')[0].strip()] = open(txt).read().strip()
+        meta = txt.split('/')[-1].split('.txt')[0].strip()
+        contents = [v.strip() for v in open(txt).read().strip().splitlines()]
+        if len(contents) == 1:
+            curr[meta] = contents[0]
+        elif len(contents) > 1:
+            curr[meta] = contents
     data[serial] = curr
 f = open('data.json', 'w')
 jdump(data, f)
