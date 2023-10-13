6799d741a09653c4b3a7e0a5ac85092bf9306e1c
Niema Moshiri <niemamoshiri@gmail.com>
2023-10-13 15:12:18 -0700

    Create create_complete_data_json.py

---
+++ scripts/create_complete_data_json.py
@@ -0,0 +1,16 @@
+#! /usr/bin/env python3
+'''
+Build a complete data.json file from game folders
+'''
+from glob import glob
+from json import dump as jdump
+data = dict()
+for game_folder in glob('games/*'):
+    serial = game_folder.rstrip('/')[-1].strip()
+    curr = dict()
+    for txt in glob('%s/*.txt' % game_folder):
+        curr[txt.split('/')[-1].split('.txt')[0].strip()] = open(txt).read().strip()
+    data[serial] = curr
+f = open('data.json', 'w')
+jdump(data, f)
+f.close()
